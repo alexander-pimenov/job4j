b484d73ac8b3299188a36edcb7bf2f46cb596d04
Alexander Pimenov <pimalex1978@yandex.ru>
2019-02-22 23:09:46 +0300

    1.1. РЎРѕР·РґР°РЅРёРµ Hello World[#141]

---
+++ src/ru/job4j/calculate/Calculate.java
@@ -0,0 +1,7 @@
+package ru.job4j.calculate; //ru.job4j.calculate - €вл€етс€ элементом программы, показывающим полный путь
+public class Calculate {    //название класса, в котором набор команд описывающих поведение программы, код
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+}
