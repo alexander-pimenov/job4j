86d38635f43e030c91acc973311ad9ac169bfe1a
Alexander Pimenov <pimalex1978@yandex.ru>
2019-03-11 19:22:27 +0300

    1.3. JavaDoc. Code convention [#115099]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,18 @@
 package ru.job4j.calculate; //ru.job4j.calculate - является элементом программы, показывающим полный путь
+/**
+* Описание Класса
+* Class Calculate для вычисления арифметических операций + - * / 
+* @author parsentev
+* @since 11.03.2019
+* @version 1.0
+*/
 public class Calculate {    //название класса, в котором набор команд описывающих поведение программы, код
+
+/**
+* Оптсание Метода
+* Конструктор, вывод строки в консоль
+* @param args = args
+*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
