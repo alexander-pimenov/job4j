da0ce695066f30bd2143092ae0ef7cd7939c9043
Alexander Pimenov <pimalex1978@yandex.ru>
2019-03-11 11:49:04 +0300

    1.2. РџРѕРґРєР»СЋС‡РёС‚СЊ СЃР±РѕСЂС‰РёРє Maven [#4086]

--- src/ru/job4j/calculate/Calculate.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.calculate; //ru.job4j.calculate - €вл€етс€ элементом программы, показывающим полный путь
-public class Calculate {    //название класса, в котором набор команд описывающих поведение программы, код
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-	
-}
