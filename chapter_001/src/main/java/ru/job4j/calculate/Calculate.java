f566b99664f91923142419dd9685525c0d55c72a
Alexander Pimenov <pimalex1978@yandex.ru>
2019-03-12 12:12:03 +0300

    1.4. JUnit [#4122] Рё CalculateTest

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,20 +1,29 @@
 package ru.job4j.calculate; //ru.job4j.calculate - является элементом программы, показывающим полный путь
 /**
-* Описание Класса
-* Class Calculate для вычисления арифметических операций + - * / 
-* @author parsentev
-* @since 11.03.2019
-* @version 1.0
-*/
+ * Описание Класса
+ * Class Calculate для вычисления арифметических операций + - * / 
+ * @author parsentev (parsentev@yandex.ru)
+ * @since 11.03.2019
+ * @version 1.0
+ */
 public class Calculate {    //название класса, в котором набор команд описывающих поведение программы, код
 
 /**
-* Оптсание Метода
-* Конструктор, вывод строки в консоль
-* @param args = args
-*/
+ * Оптсание Метода
+ * Конструктор, вывод строки в консоль
+ * @param args = args
+ */
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
+	
+/**
+ * Method echo.
+ * @param name Your name.
+ * @return Echo plus your name.
+ */
+ public String echo(String name) {
+    return "Echo, echo, echo : " + name;
+ }
 	
 }
