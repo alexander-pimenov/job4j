cea7a1e447c6de250f8b6ec5b71b50e02597126b
Alexander Pimenov <pimalex1978@yandex.ru>
2019-03-16 17:19:33 +0300

    3.1. Элементарный калькулятор.[#115090]

---
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+
+    //метод складывает параметр first и second и возвращает значение.
+    public double add(double first, double second) {
+        return first + second;
+    }
+    //метод вычитает параметр first и second и возвращает значение.
+    public double subtract(double first, double second) {
+        return first - second;
+    }
+    //метод делит параметр first и second и возвращает значение.
+    public double div(double first, double second) {
+        return first / second;
+    }
+    //метод умножает параметр first и second и возвращает значение.
+    public double multiple(double first, double second) {
+        return first * second;
+    }
+
+}
