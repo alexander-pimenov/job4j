7add48dabbc02c2ebbee4bd9173f9593e17c11b5
Alexander Pimenov <pimalex1978@yandex.ru>
2019-03-26 19:34:21 +0300

    5.5. Рефакторинг кода.[#115068]

--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -3,8 +3,10 @@ package ru.job4j.loop;
 import org.junit.Test;
 //Этот класс выполняет такие же задачи, как и класс StringBuilder, но он имеет более удобный интерфейс использования.
 import java.util.StringJoiner;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
+
 /**
  * @author Petr Arsentev (parsentev@yandex.ru)
  * @version $Id$
@@ -13,6 +15,40 @@ import static org.hamcrest.Matchers.is;
 public class PaintTest {
     @Test
     public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenPyramid4() {
         Paint paint = new Paint();
         String rst = paint.pyramid(4);
         System.out.println(rst);
