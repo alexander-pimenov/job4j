2b07cc8fe55f794f3a46a0fef7ba46d9d8c4f28d
Alexander Pimenov <pimalex1978@yandex.ru>
2019-04-13 21:27:15 +0300

    Экзамен. Базовый синтаксис.[#115106] Добавил методы в задачу по слиянию двух отсортированных массивов

--- chapter_001/src/test/java/ru/job4j/array/ArraySortTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArraySortTest.java
@@ -33,4 +33,54 @@ public class ArraySortTest {
         int[] result = newAr.merge(a, b);
         assertThat(result, is(expect));
     }
+
+    @Test
+    /*
+     * */
+    public void whenSortedArrayPlusOtherSortedArraysGetThirdSortedArray3() {
+        int[] a = {1, 1, 1, 1};
+        int[] b = {2, 2, 2, 2};
+        int[] expect = {1, 1, 1, 1, 2, 2, 2, 2};
+        ArraySort newAr = new ArraySort();
+        int[] result = newAr.merge(a, b);
+        assertThat(result, is(expect));
+    }
+    /*
+     * здесь тест проверяет слияние двух отсортированных массивов
+     * первый массив меньше второго
+     * */
+    @Test
+    public void whenSortedArrayPlusOtherSortedArraysGetThirdSortedArray4() {
+        int[] a = {1, 3, 5};
+        int[] b = {2, 4, 6, 8};
+        int[] expect = {1, 2, 3, 4, 5, 6, 8};
+        ArraySort newAr = new ArraySort();
+        int[] result = newAr.merge(a, b);
+        assertThat(result, is(expect));
+    }
+    /*
+     * здесь тест проверяет слияние двух отсортированных массивов
+     * первый массив больше второго
+     * */
+    @Test
+    public void whenSortedArrayPlusOtherSortedArraysGetThirdSortedArray5() {
+        int[] a = {1, 3, 5, 7, 9};
+        int[] b = {2, 4, 6};
+        int[] expect = {1, 2, 3, 4, 5, 6, 7, 9};
+        ArraySort newAr = new ArraySort();
+        int[] result = newAr.merge(a, b);
+        assertThat(result, is(expect));
+    }
+    /*
+    * здесь тест проверяет слияние двух пустых массивов
+    * */
+    @Test
+    public void whenSortedArrayPlusOtherSortedArraysGetThirdSortedArray6() {
+        int[] a = {};
+        int[] b = {};
+        int[] expect = {};
+        ArraySort newAr = new ArraySort();
+        int[] result = newAr.merge(a, b);
+        assertThat(result, is(expect));
+    }
 }
