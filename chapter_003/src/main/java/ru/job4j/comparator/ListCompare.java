e1490a5978c9044f182aa58f2ab9ae0a1ec73415
Alexander Pimenov <pimalex1978@yandex.ru>
2019-08-30 23:01:30 +0300

    3. Компаратор для строк.[#115113]

---
+++ chapter_003/src/main/java/ru/job4j/comparator/ListCompare.java
@@ -0,0 +1,35 @@
+package ru.job4j.comparator;
+
+import java.util.Comparator;
+
+/*
+ * Необходимо реализовать поэлементное сравнение двух списков,
+ * т.е. сравниваем элементы двух списков, находящихся на одних
+ * и тех же позициях (по одним и тем же индексом). Сравнение
+ * в лексикографическом порядке.
+ */
+
+public class ListCompare implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+
+        int result = 0;
+        int lim = Math.min(left.length(), right.length());
+        for (int index = 0; index < lim; index++) {
+            result = Character.compare(left.charAt(index), right.charAt(index));
+            if (result < 0) {
+                result = -1;
+                break;
+            }
+            if (result > 0) {
+                result = 1;
+                break;
+            }
+        }
+        if (result == 0) {
+            result = Integer.compare(left.length(), right.length());
+        }
+        return result;
+    }
+}
