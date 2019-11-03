2032d11a39dde91f882fdf67405d5d6e9c850a20
Alexander Pimenov <pimalex1978@yandex.ru>
2019-11-03 19:50:53 +0400

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#115025]: Добавлены методы со stream, а старые закоментированны

--- chapter_003/src/main/java/ru/job4j/bank/Bank.java
+++ chapter_003/src/main/java/ru/job4j/bank/Bank.java
@@ -18,16 +18,16 @@ public class Bank {
         this.userAccounts.putIfAbsent(user, new ArrayList<>());
     }
 
-    public User getUser(String passport) {
-        User result = null;
-        for (User user : this.userAccounts.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                result = user;
-                break; //чтобы не вертеть цикл дальше
-            }
-        }
-        return result;
-    }
+//    public User getUser(String passport) {
+//        User result = null;
+//        for (User user : this.userAccounts.keySet()) {
+//            if (user.getPassport().equals(passport)) {
+//                result = user;
+//                break; //чтобы не вертеть цикл дальше
+//            }
+//        }
+//        return result;
+//    }
 
     public User getUser1(String passport) {
         return this.userAccounts.keySet().stream()
@@ -41,14 +41,14 @@ public class Bank {
     }
 
     /*Добавить счет пользователю.*/
-    public void addAccountToUser(String passport, Account account) {
-        for (User user : this.userAccounts.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                this.userAccounts.get(user).add(account);
-                break; //чтобы не вертеть цикл дальше
-            }
-        }
-    }
+//    public void addAccountToUser(String passport, Account account) {
+//        for (User user : this.userAccounts.keySet()) {
+//            if (user.getPassport().equals(passport)) {
+//                this.userAccounts.get(user).add(account);
+//                break; //чтобы не вертеть цикл дальше
+//            }
+//        }
+//    }
 
     public void addAccountToUser1(String passport, Account account) {
         this.getUserAccounts1(passport).add(account);
@@ -56,14 +56,14 @@ public class Bank {
 
 
     /*Удалить один счет пользователя.*/
-    public void deleteAccountFromUser(String passport, Account account) {
-        for (User user : this.userAccounts.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                this.userAccounts.get(user).remove(account);
-                break; //чтобы не вертеть цикл дальше
-            }
-        }
-    }
+//    public void deleteAccountFromUser(String passport, Account account) {
+//        for (User user : this.userAccounts.keySet()) {
+//            if (user.getPassport().equals(passport)) {
+//                this.userAccounts.get(user).remove(account);
+//                break; //чтобы не вертеть цикл дальше
+//            }
+//        }
+//    }
 
     public void deleteAccountFromUser1(String passport, Account account) {
         this.getUserAccounts1(passport).remove(account);
@@ -71,15 +71,15 @@ public class Bank {
 
 
     /*Получить список счетов для пользователя по данным паспорта.*/
-    public List<Account> getUserAccounts(String passport) {
-        List<Account> result = new ArrayList<>();
-        for (User user : this.userAccounts.keySet()) {
-            if (user.getPassport().equals(passport)) {
-                result = this.userAccounts.get(user);
-            }
-        }
-        return result;
-    }
+//    public List<Account> getUserAccounts(String passport) {
+//        List<Account> result = new ArrayList<>();
+//        for (User user : this.userAccounts.keySet()) {
+//            if (user.getPassport().equals(passport)) {
+//                result = this.userAccounts.get(user);
+//            }
+//        }
+//        return result;
+//    }
 
     public List<Account> getUserAccounts1(String passport) {
         return this.userAccounts.get(findUser1(passport));
@@ -87,17 +87,17 @@ public class Bank {
 
 
     /*Поиск пользователя по данным паспорта*/
-    private User findUser(String passport) {
-        User result = null;
-        Set<User> keys = this.userAccounts.keySet();
-        for (User key : keys) {
-            if (key != null && key.getPassport().equals(passport)) {
-                result = key;
-                break;
-            }
-        }
-        return result;
-    }
+//    private User findUser(String passport) {
+//        User result = null;
+//        Set<User> keys = this.userAccounts.keySet();
+//        for (User key : keys) {
+//            if (key != null && key.getPassport().equals(passport)) {
+//                result = key;
+//                break;
+//            }
+//        }
+//        return result;
+//    }
 
     private User findUser1(String passport) {
         Stream<User> userStream = this.userAccounts.keySet().stream();
@@ -106,16 +106,16 @@ public class Bank {
     }
 
     /*Поиск аккаунта (одного счета) пользователя по данным паспорта и реквизитов счета*/
-    private Account findAccount(String passport, String requisite) {
-        Account resultAccount = null;
-        for (Account account : getUserAccounts(passport)) {
-            if (account.getRequisites().equals(requisite)) {
-                resultAccount = account;
-                break; //чтобы не вертеть цикл дальше
-            }
-        }
-        return resultAccount;
-    }
+//    private Account findAccount(String passport, String requisite) {
+//        Account resultAccount = null;
+//        for (Account account : getUserAccounts(passport)) {
+//            if (account.getRequisites().equals(requisite)) {
+//                resultAccount = account;
+//                break; //чтобы не вертеть цикл дальше
+//            }
+//        }
+//        return resultAccount;
+//    }
 
     private Account findAccount1(String passport, String requisite) {
 
--- chapter_003/src/test/java/ru/job4j/bank/BankTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankTest.java
@@ -12,15 +12,15 @@ public class BankTest {
 
     private Bank bank = new Bank();
 
-    @Test
-    public void whenAddUser() {
-        User petrov = new User("Petrov", "1111-5678");
-        User ivanov = new User("Ivanov", "2222-5678");
-        bank.addUser(petrov);
-        bank.addUser(ivanov);
-        assertThat(bank.getUser("1111-5678"), is(petrov));
-        assertThat(bank.getUser("2222-5678"), is(ivanov));
-    }
+//    @Test
+//    public void whenAddUser() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        User ivanov = new User("Ivanov", "2222-5678");
+//        bank.addUser(petrov);
+//        bank.addUser(ivanov);
+//        assertThat(bank.getUser("1111-5678"), is(petrov));
+//        assertThat(bank.getUser("2222-5678"), is(ivanov));
+//    }
 
     @Test
     public void whenAddUser1() {
@@ -32,14 +32,15 @@ public class BankTest {
         assertThat(bank.getUser1("2222-5678"), is(ivanov));
     }
 
-    @Test
-    public void whenDeleteUser() {
-        Bank bank = new Bank();
-        User petrov = new User("Petrov", "1111-5678");
-        bank.addUser(petrov);
-        bank.deleteUser(petrov);
-        assertThat(bank.getUser("1111-5678"), is(nullValue()));
-    }
+//    @Test
+//    public void whenDeleteUser() {
+//        Bank bank = new Bank();
+//        User petrov = new User("Petrov", "1111-5678");
+//        bank.addUser(petrov);
+//        bank.deleteUser(petrov);
+//        assertThat(bank.getUser("1111-5678"), is(nullValue()));
+//    }
+
     @Test
     public void whenDeleteUser1() {
         Bank bank = new Bank();
@@ -49,17 +50,17 @@ public class BankTest {
         assertThat(bank.getUser1("1111-5678"), is(nullValue()));
     }
 
-    @Test
-    public void whenAddTwoAccountsToUser() {
-        User petrov = new User("Petrov", "1111-5678");
-        Account first = new Account("10-20-30", 1000);
-        Account second = new Account("40-50-60", 2000);
-        bank.addUser(petrov);
-        bank.addAccountToUser("1111-5678", first);
-        bank.addAccountToUser("1111-5678", second);
-        List<Account> result = bank.getUserAccounts("1111-5678");
-        assertThat(result.size(), is(2));
-    }
+//    @Test
+//    public void whenAddTwoAccountsToUser() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        Account first = new Account("10-20-30", 1000);
+//        Account second = new Account("40-50-60", 2000);
+//        bank.addUser(petrov);
+//        bank.addAccountToUser("1111-5678", first);
+//        bank.addAccountToUser("1111-5678", second);
+//        List<Account> result = bank.getUserAccounts("1111-5678");
+//        assertThat(result.size(), is(2));
+//    }
 
     @Test
     public void whenAddTwoAccountsToUser1() {
@@ -73,22 +74,22 @@ public class BankTest {
         assertThat(result.size(), is(2));
     }
 
-    @Test
-    public void whenDeleteAccountFromUser() {
-        User petrov = new User("Petrov", "1111-5678");
-        Account first = new Account("10-20-30", 1000);
-        Account second = new Account("40-50-60", 2000);
-        Account third = new Account("70-80-90", 3000);
-        bank.addUser(petrov);
-        bank.addAccountToUser("1111-5678", first);
-        bank.addAccountToUser("1111-5678", second);
-        bank.addAccountToUser("1111-5678", third);
-        List<Account> result = bank.getUserAccounts("1111-5678");
-        assertThat(result.size(), is(3));
-        bank.deleteAccountFromUser("1111-5678", first);
-        result = bank.getUserAccounts("1111-5678");
-        assertThat(result.size(), is(2));
-    }
+//    @Test
+//    public void whenDeleteAccountFromUser() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        Account first = new Account("10-20-30", 1000);
+//        Account second = new Account("40-50-60", 2000);
+//        Account third = new Account("70-80-90", 3000);
+//        bank.addUser(petrov);
+//        bank.addAccountToUser("1111-5678", first);
+//        bank.addAccountToUser("1111-5678", second);
+//        bank.addAccountToUser("1111-5678", third);
+//        List<Account> result = bank.getUserAccounts("1111-5678");
+//        assertThat(result.size(), is(3));
+//        bank.deleteAccountFromUser("1111-5678", first);
+//        result = bank.getUserAccounts("1111-5678");
+//        assertThat(result.size(), is(2));
+//    }
 
     @Test
     public void whenDeleteAccountFromUser1() {
@@ -107,19 +108,19 @@ public class BankTest {
         assertThat(result.size(), is(2));
     }
 
-    @Test
-    public void whenGetUserAccounts() {
-        User petrov = new User("Petrov", "1111-5678");
-        Account first = new Account("10-20-30", 1000);
-        Account second = new Account("40-50-60", 2000);
-        Account third = new Account("70-80-90", 3000);
-        bank.addUser(petrov);
-        bank.addAccountToUser("1111-5678", first);
-        bank.addAccountToUser("1111-5678", second);
-        bank.addAccountToUser("1111-5678", third);
-        List<Account> result = bank.getUserAccounts("1111-5678");
-        assertThat(result.size(), is(3));
-    }
+//    @Test
+//    public void whenGetUserAccounts() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        Account first = new Account("10-20-30", 1000);
+//        Account second = new Account("40-50-60", 2000);
+//        Account third = new Account("70-80-90", 3000);
+//        bank.addUser(petrov);
+//        bank.addAccountToUser("1111-5678", first);
+//        bank.addAccountToUser("1111-5678", second);
+//        bank.addAccountToUser("1111-5678", third);
+//        List<Account> result = bank.getUserAccounts("1111-5678");
+//        assertThat(result.size(), is(3));
+//    }
 
     @Test
     public void whenGetUserAccounts1() {
@@ -136,29 +137,29 @@ public class BankTest {
 
     }
 
-    @Test
-    public void whenTransferMoney() {
-        User petrov = new User("Petrov", "1111-5678");
-        User ivanov = new User("Ivanov", "2222-5678");
-        Account first = new Account("10-20-30", 1000);
-        Account second = new Account("40-50-60", 2000);
-        bank.addUser(petrov);
-        bank.addUser(ivanov);
-        Account third = new Account("70-80-90", 3000);
-        bank.addAccountToUser("1111-5678", first);
-        bank.addAccountToUser("1111-5678", second);
-        bank.addAccountToUser("2222-5678", third);
-        boolean result = bank.transferMoney("1111-5678", "10-20-30", "2222-5678", "70-80-90", 300);
-
-        List<Account> srcUserAccounts = bank.getUserAccounts("1111-5678");
-        List<Account> destUserAccounts = bank.getUserAccounts("2222-5678");
-
-        double srcValue = srcUserAccounts.get(0).getValue();
-        double destValue = destUserAccounts.get(0).getValue();
-        assertThat(srcValue, is(700.0));
-        assertThat(destValue, is(3300.0));
-        assertThat(result, is(true));
-    }
+//    @Test
+//    public void whenTransferMoney() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        User ivanov = new User("Ivanov", "2222-5678");
+//        Account first = new Account("10-20-30", 1000);
+//        Account second = new Account("40-50-60", 2000);
+//        bank.addUser(petrov);
+//        bank.addUser(ivanov);
+//        Account third = new Account("70-80-90", 3000);
+//        bank.addAccountToUser("1111-5678", first);
+//        bank.addAccountToUser("1111-5678", second);
+//        bank.addAccountToUser("2222-5678", third);
+//        boolean result = bank.transferMoney("1111-5678", "10-20-30", "2222-5678", "70-80-90", 300);
+//
+//        List<Account> srcUserAccounts = bank.getUserAccounts("1111-5678");
+//        List<Account> destUserAccounts = bank.getUserAccounts("2222-5678");
+//
+//        double srcValue = srcUserAccounts.get(0).getValue();
+//        double destValue = destUserAccounts.get(0).getValue();
+//        assertThat(srcValue, is(700.0));
+//        assertThat(destValue, is(3300.0));
+//        assertThat(result, is(true));
+//    }
 
     @Test
     public void whenTransferMoney1() {
@@ -185,24 +186,24 @@ public class BankTest {
     }
 
 
-    @Test
-    public void whenTransferMoneyFromOneUsersAccountToAnother() {
-        User petrov = new User("Petrov", "1111-5678");
-        Account first = new Account("10-20-30", 1000);
-        Account second = new Account("40-50-60", 2000);
-        bank.addUser(petrov);
-        bank.addAccountToUser("1111-5678", first);
-        bank.addAccountToUser("1111-5678", second);
-        bank.transferMoney("1111-5678", "10-20-30", "1111-5678", "40-50-60", 300);
-
-        List<Account> userAccounts = bank.getUserAccounts("1111-5678");
-
-        double srcValue = userAccounts.get(0).getValue();
-        double destValue = userAccounts.get(1).getValue();
-
-        assertThat(srcValue, is(700.0));
-        assertThat(destValue, is(2300.0));
-    }
+//    @Test
+//    public void whenTransferMoneyFromOneUsersAccountToAnother() {
+//        User petrov = new User("Petrov", "1111-5678");
+//        Account first = new Account("10-20-30", 1000);
+//        Account second = new Account("40-50-60", 2000);
+//        bank.addUser(petrov);
+//        bank.addAccountToUser("1111-5678", first);
+//        bank.addAccountToUser("1111-5678", second);
+//        bank.transferMoney("1111-5678", "10-20-30", "1111-5678", "40-50-60", 300);
+//
+//        List<Account> userAccounts = bank.getUserAccounts("1111-5678");
+//
+//        double srcValue = userAccounts.get(0).getValue();
+//        double destValue = userAccounts.get(1).getValue();
+//
+//        assertThat(srcValue, is(700.0));
+//        assertThat(destValue, is(2300.0));
+//    }
 
     @Test
     public void whenTransferMoneyFromOneUsersAccountToAnother1() {
