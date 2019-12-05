ee610d33cae3e1b80afcf04784324fd5ee424f45
Alexander Pimenov <pimalex1978@yandex.ru>
2019-12-05 16:38:31 +0300

    Небольшие исправления в коассе stream/Count

--- chapter_004/src/main/java/ru/job4j/stream/Count.java
+++ chapter_004/src/main/java/ru/job4j/stream/Count.java
@@ -10,7 +10,7 @@ import java.util.List;
  */
 public class Count {
 
-    public int count1(List<Integer> data) {
+    public static int count1(List<Integer> data) {
         return data.stream()
                 .filter(x -> x % 2 == 0)
                 .map(x -> x * x)
@@ -31,7 +31,7 @@ public class Count {
                 .sum();
     }
 
-//    public static void main(String[] args) {
+//    public void main(String[] args) {
 //        List<Integer> data = List.of(1, 2, 3, 4, 5);
 //
 //        System.out.println(count1(data));
