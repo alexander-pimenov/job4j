3ee9d6ba99582432ec9fe2351ddbea93059f653f
Alexander Pimenov <pimalex1978@yandex.ru>
2019-12-16 19:07:15 +0300

    5.1.4. Создать convert(Iterator<Iterator>)[#210777]

---
+++ chapter_005/src/main/java/ru/job4j/iterator/IteratorOfIteratorsConverter.java
@@ -0,0 +1,47 @@
+package ru.job4j.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Класс IteratorOfIteratorsConverter с методом converter()
+ * для преобразования Итератора итераторов в Итератор чисел,
+ * т.е. на вход метод получает объект Итератор итераторов
+ * и возвращает Итератор чисел.
+ * Например,
+ * На вход подаем Iterator<Iterator<Integer>> - ((4 2 0 4 6 4 9),(0 9 8 7 5),(1 3 5 6 7 0 9 8 4))
+ * Метод возвращает Iterator<Integer> - (4 2 0 4 6 4 9 0 9 8 7 5 1 3 5 6 7 0 9 8 4)
+ */
+
+
+public class IteratorOfIteratorsConverter {
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            Iterator<Integer> inner = it.next();
+
+            @Override
+            public boolean hasNext() {
+                if (inner == null) {
+                    return false;
+                }
+                while (!inner.hasNext() && it.hasNext()) {
+                    inner = it.next();
+                }
+                return inner.hasNext();
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                if (inner.hasNext()) {
+                    return inner.next();
+                } else {
+                    inner = it.next();
+                    return inner.next();
+                }
+            }
+        };
+    }
+}
