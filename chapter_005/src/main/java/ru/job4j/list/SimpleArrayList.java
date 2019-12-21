7a9c4652d98505ebf5ba977e403e2f8cf4f61874
Alexander Pimenov <pimalex1978@yandex.ru>
2019-12-21 12:17:31 +0300

    5.3.0 Создать метод delete для односвязного списка[#210768]

---
+++ chapter_005/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,102 @@
+package ru.job4j.list;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Контейнер для данных, базирующийся на односвязном списке.
+ * Класс SimpleArrayList.
+ */
+
+public class SimpleArrayList<E> implements Iterable<E>{
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в НАЧАЛО списка данные.
+     * (очень похоже на Stack - добавляем в начало.)
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Метод реализующий удаления ПЕРВОГО элемента в списке
+     * и возвращающий его значение.
+     * (очень похоже на Stack - удаляем из начала.)
+     */
+    public E delete() {
+        Node<E> result = this.first;
+        if (result == null) {
+            throw new NoSuchElementException();
+        }
+        this.first = first.next;
+        this.size--;
+        return result.data;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        if (result == null) {
+            throw new NoSuchElementException();
+        }
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Метод получения Итератора.
+     */
+
+    @Override
+    public Iterator<E> iterator() {
+        return new Iterator<>() {
+            private int cursor = 0;
+
+            @Override
+            public boolean hasNext() {
+                return cursor < size;
+            }
+
+            @Override
+            public E next() {
+                if (!hasNext()) {
+                    throw new IndexOutOfBoundsException();
+                }
+
+                int i = cursor;
+                E nextElement = get(i);
+                cursor++;
+                return nextElement;
+            }
+        };
+    }
+
+    /**
+     * Класс предназначенный для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
