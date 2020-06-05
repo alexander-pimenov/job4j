4fd13d7ba478e6687c8dae0eb80ad396a702cfb6
Alexander Pimenov <pimalex1978@yandex.ru>
2020-06-05 17:08:53 +0300

    1.2. Режим ожидания.[231217#301980]

---
+++ chapter_007/src/main/java/ru/job4j/concurrent/Wget.java
@@ -0,0 +1,26 @@
+package ru.job4j.concurrent;
+
+public class Wget {
+    public static void main(String[] args) {
+        Thread thread = new Thread(
+                () -> {
+                    try {
+                        for (int i = 0; i <= 101; i++) {
+                            if (i != 101) {
+                                System.out.print("\rLoading : " + i + "%");
+                            }
+                            else {
+                                //Небольшая задержка перед выводом результата
+                                Thread.sleep(1000);
+                                System.out.print("\rLoaded successfully.");
+                            }
+                            Thread.sleep(100);
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+        );
+        thread.start();
+    }
+}
