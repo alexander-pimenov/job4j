a2a4da1abd6e7e0ca64a15f803cb8b1e127c3a72
Alexander Pimenov <pimalex1978@yandex.ru>
2020-12-11 15:12:19 +0300

    0. Объект монитор, критическая секция[6857#301996]

--- chapter_009_/src/main/java/ru/job4j/synch/Count.java
+++ chapter_009_/src/main/java/ru/job4j/synch/Count.java
@@ -1,13 +1,35 @@
 package ru.job4j.synch;
 
+/*
+ * Объект монитор - это объект, по которому виртуальная машина понимает,
+ * что блок кода выполняется нитью.
+ * Объект монитора может быть в двух значениях - блок занят или блок
+ * свободный. Проверка состояния монитора атомарна.
+ * Блок synchronized() {} называется критической секцией. В ней
+ * одновременно может находиться только одна нить.
+ */
 public class Count {
     private int value;
 
-    public synchronized void increment() {
-        value++;
+    public void increment() {
+        synchronized (this) {
+            value++;
+        }
     }
 
-    public synchronized int get() {
-        return value;
+    //Эквивалентная запись: с ключевым словом synchronized
+//    public synchronized void increment() {
+//        value++;
+//    }
+
+    public int get() {
+        synchronized (this) {
+            return value;
+        }
     }
+
+    //Эквивалентная запись: с ключевым словом synchronized
+//    public synchronized int get() {
+//        return value;
+//    }
 }
