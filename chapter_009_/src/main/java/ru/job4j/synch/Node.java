95b8dbc0164a51f64242294ed6dca9c9a1577c2a
Alexander Pimenov <pimalex1978@yandex.ru>
2020-12-11 13:27:38 +0300

    3. Immutable объекты.[267918#301987]

---
+++ chapter_009_/src/main/java/ru/job4j/synch/Node.java
@@ -0,0 +1,23 @@
+package ru.job4j.synch;
+
+import javax.annotation.concurrent.Immutable;
+//import net.jcip.annotations.Immutable;
+
+@Immutable
+public class Node<T> {
+    private final Node next;
+    private final T value;
+
+    public Node(Node next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
