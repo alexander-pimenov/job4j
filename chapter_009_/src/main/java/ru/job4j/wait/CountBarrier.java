f29d6ecc254d5c1cb4efb6f38c4a62f84e293696
Alexander Pimenov <pimalex1978@yandex.ru>
2020-12-16 19:48:49 +0300

    0. Управление нитью через wait.[6858#301976] CountBarrier

---
+++ chapter_009_/src/main/java/ru/job4j/wait/CountBarrier.java
@@ -0,0 +1,75 @@
+package ru.job4j.wait;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public class CountBarrier {
+    @GuardedBy("lock")
+    private final Object lock = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (lock) {
+
+            final String currentName = Thread.currentThread().getName();
+            System.out.println(String.format("%s started....", currentName));
+            count++;
+            System.out.println(String.format("%s count = %d", currentName, count));
+            lock.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (lock) {
+            final String currentName = Thread.currentThread().getName();
+
+            System.out.println(String.format("%s started....", currentName));
+            try {
+                while (count < total) {
+                    System.out.println(String.format("%s is waiting...", currentName));
+                    lock.wait();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+            System.out.println(String.format("%s resumed!", currentName));
+        }
+    }
+
+    public static void main(String[] args) {
+        int totalCall = 10;
+        CountBarrier countBarrier = new CountBarrier(totalCall);
+        Thread th1 = new Thread(
+                () -> {
+                    System.out.println("-= " + Thread.currentThread().getName() + " =-");
+                    for (int i = 0; i < totalCall; i++) {
+                        countBarrier.count();
+                    }
+                }, "First"
+        );
+        Thread th2 = new Thread(
+                () -> {
+                    System.out.println("-= " + Thread.currentThread().getName() + " =-");
+                    countBarrier.await();
+                }, "Second"
+        );
+
+        th1.start();
+        th2.start();
+
+        try {
+            th1.join();
+            th2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
