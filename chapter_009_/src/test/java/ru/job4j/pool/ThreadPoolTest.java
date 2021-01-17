ab42cbdaf2c0624672668944748d2646b2ee2a95
Alexander Pimenov <pimalex1978@yandex.ru>
2021-01-17 19:42:11 +0300

    Реализовать ThreadPool[1099#301971] добавлен метод в ThreadPoolTest

--- chapter_009_/src/test/java/ru/job4j/pool/ThreadPoolTest.java
+++ chapter_009_/src/test/java/ru/job4j/pool/ThreadPoolTest.java
@@ -8,7 +8,7 @@ public class ThreadPoolTest {
     @Test
     public void threadPoolTest() {
         ThreadPool threadPool = new ThreadPool();
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 5; i++) {
             int finalI = i;
             threadPool.work(() ->
                     System.out.println(
@@ -27,6 +27,29 @@ public class ThreadPoolTest {
             e.printStackTrace();
         }
         //закрываем ThreadPool
+        threadPool.shutdown();
+    }
+
+    @Test
+    public void threadPoolTestOneRunnableObject() {
+        Runnable task1 = () -> {
+            System.out.println("Executing Task1 inside : " + Thread.currentThread().getName());
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException ignore) {
+            }
+            System.out.println("My task1 end..");
+        };
+
+        ThreadPool threadPool = new ThreadPool();
+
+        threadPool.work(task1);
+        //ждем 5 секунд
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         threadPool.shutdown();
     }
 }
